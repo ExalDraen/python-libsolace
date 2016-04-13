a31dffe9816681fed1af6ca894a290ec11ebc21e
Kegan Holtzhausen <kegan.holtzhausen@unibet.com>
2016-04-13 10:19:06 +0200

    monitor sh

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -2,11 +2,12 @@
 
 while true; do
 
-./bin/solace-metrics.py -e prod --clients
+./bin/solace-metrics.py -e prod --clients &
 ./bin/solace-metrics.py -e prod --vpns
 ./bin/solace-metrics.py -e prod --spool
+./bin/solace-metrics.py -e prod --client-spools
 #./bin/solace-list-vpns.py -e prod | xargs -i{} ./bin/solace-metrics.py -e prod --spool --filter {}
-
-sleep 60
+echo "sleeping"
+sleep 10
 done;
 
