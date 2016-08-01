b0d6eaef65e34b1374ff645251cdfa6133871054
Kegan Holtzhausen <Kegan.Holtzhausen@UNIBET.com>
2016-08-01 14:02:18 +0200

    cicd scripts

--- cicd.sh
+++ cicd.sh
@@ -2,3 +2,6 @@
 
 /usr/bin/sudo apt-get update
 /usr/bin/sudo apt-get -y install python-virtualenv git libxslt1-dev libxml2-dev python-dev
+/usr/bin/virtualenv ~/py27/bin/python
+source ~/py27/bin/activate
+/usr/bin/env python setup.py install
