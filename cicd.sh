70b46cbcdb9b5f6960414954e3b7e84219e771a6
Kegan Holtzhausen <Kegan.Holtzhausen@UNIBET.com>
2016-08-01 14:24:54 +0200

    dont deleate py27 for now

--- cicd.sh
+++ cicd.sh
@@ -2,10 +2,11 @@
 
 /usr/bin/sudo apt-get update
 /usr/bin/sudo apt-get -y install python-virtualenv git libxslt1-dev libxml2-dev python-dev libyaml-dev lib32z1-dev
-rm -R -f ~/py27
+#rm -R -f ~/py27
 /usr/bin/virtualenv ~/py27
 source ~/py27/bin/activate
 /usr/bin/env python setup.py install
+pip install --upgrade pip
 pip install nose
 pip install coverage
 chmod 0644 ~/.python-eggs
