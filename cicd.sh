4c037876f110b391133b8b8b7f8a89c302c5ab3e
Kegan Holtzhausen <Kegan.Holtzhausen@UNIBET.com>
2016-08-01 14:08:31 +0200

    remove old py27

--- cicd.sh
+++ cicd.sh
@@ -2,6 +2,7 @@
 
 /usr/bin/sudo apt-get update
 /usr/bin/sudo apt-get -y install python-virtualenv git libxslt1-dev libxml2-dev python-dev
-/usr/bin/virtualenv ~/py27/bin/python
+rm -R -f ~/py27
+/usr/bin/virtualenv ~/py27
 source ~/py27/bin/activate
 /usr/bin/env python setup.py install
